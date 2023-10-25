98fa4eedd749d7949c185a440bb8200607e10576
johnytoxic <johnytoxic@protonmail.com>
2023-10-25 20:49:06 +0200

    disable Steam Rich Presence

---
+++ CSL2SteamPrivacy/RichPresenceUpdateSystemPatch.cs
@@ -0,0 +1,14 @@
+﻿using Colossal.PSI.Steamworks;
+using HarmonyLib;
+
+namespace CSL2SteamPrivacy
+{
+    [HarmonyPatch(typeof(SteamworksPlatform), nameof(SteamworksPlatform.SetRichPresence))]
+    public class SetRichPresencePatch
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
--- CSL2SteamPrivacy/SteamPrivacyMod.cs
+++ CSL2SteamPrivacy/SteamPrivacyMod.cs
@@ -4,5 +4,10 @@ namespace CSL2SteamPrivacy
 {
     public class SteamPrivacyMod
     {
+        public static void Setup()
+        {
+            var harmony = new Harmony("net.johnytoxic.CSL2SteamPrivacy");
+            harmony.PatchAll();
+        }
     }
 }
