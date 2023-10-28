7ad36d8834c14784423e1dacf560ab122d168d8f
johnytoxic <johnytoxic@protonmail.com>
2023-10-28 20:35:24 +0200

    fix patching of library method

--- CSL2SteamPrivacy/RichPresenceUpdateSystemPatch.cs
+++ CSL2SteamPrivacy/RichPresenceUpdateSystemPatch.cs
@@ -1,9 +1,13 @@
-﻿using Colossal.PSI.Steamworks;
+﻿using System;
+using Colossal.PSI.Steamworks;
 using HarmonyLib;
 
 namespace CSL2SteamPrivacy
 {
-    [HarmonyPatch(typeof(SteamworksPlatform), nameof(SteamworksPlatform.SetRichPresence))]
+    [HarmonyPatch(typeof(SteamworksPlatform), nameof(SteamworksPlatform.SetRichPresence),
+        new Type[]{typeof(string)})]
+    [HarmonyPatch(typeof(SteamworksPlatform), nameof(SteamworksPlatform.SetRichPresence),
+        new Type[]{typeof(string), typeof(string)})]
     public class SetRichPresencePatch
     {
         static bool Prefix()
